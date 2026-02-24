728d9c909332ee495a7864cc4994d2ec75cc04ea
Pablo Eduardo <pabloeduardossantos@gmail.com>
2026-02-24 17:05:48 -0300

    feat: implementado documentação swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:4000',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    },
+  });
+  const config = new DocumentBuilder()
+    .setTitle('Dashify API')
+    .setDescription('API para utilização do sistema e-Quali')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
